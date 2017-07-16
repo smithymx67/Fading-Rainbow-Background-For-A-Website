a822e359adf23ca20feb3da31b512cfcd1dac15b
Sam Smith <smithymx67@users.noreply.github.com>
2017-07-16 12:57:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var counter = 0;
+var colors = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#00FFFF", "#0000FF", "#8B00FF"];
+
+function setup() {
+	setInterval(changeColor, 1500);
+}
+
+function changeColor() {
+	var body = document.body;
+	body.style.backgroundColor = colors[counter];
+
+	if(counter == 6) {
+		counter = 0;
+	} else {
+		counter++;
+	}
+}
